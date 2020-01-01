21027f828365322df6d2a361140231b11e08b23c
Julius <30121440+julius-d@users.noreply.github.com>
2020-01-01 11:52:43 +0100

    Moved RuleOrchestration out of sudoku.worker

--- src/sudoku.worker.js
+++ src/sudoku.worker.js
@@ -1,31 +1,9 @@
-import BoxCantBeRule from "./finder/numberFound2cantBe/BoxCantBeRule";
 import SudokuEvent from "./sudoku/SudokuEvent";
 import SudokuPosition from "./sudoku/SudokuPosition";
-import HorizontalCantBeRule from "./finder/numberFound2cantBe/HorizontalCantBeRule";
-import VerticalCantBeRule from "./finder/numberFound2cantBe/VerticalCantBeRule";
-import OnePositonCantBeRule from "./finder/numberFound2cantBe/OnePositonCantBeRule";
 import SudokuEventType from "./sudoku/SudokuEventType";
-import OnlyOnePlaceHorizontalLine from "./finder/cantBe2NumberFound/OnlyOnePlaceHorizontalLine";
-import OnlyOnePlaceVerticalLine from "./finder/cantBe2NumberFound/OnlyOnePlaceVerticalLine";
-import OnlyOnePlaceBox from "./finder/cantBe2NumberFound/OnlyOnePlaceBox";
-import OnePositionFinder from "./finder/cantBe2NumberFound/OnePositionFinder";
-import EventFilter from "./sudoku/EventFilter";
+import RuleOrchestration from "./sudoku/RuleOrchestration";
 
-let numberFound2cantBeRules = [
-  new BoxCantBeRule(),
-  new HorizontalCantBeRule(),
-  new VerticalCantBeRule(),
-  new OnePositonCantBeRule()
-];
-
-let cantBe2NumberFound = [
-  new OnlyOnePlaceHorizontalLine(),
-  new OnlyOnePlaceVerticalLine(),
-  new OnlyOnePlaceBox(),
-  new OnePositionFinder()
-];
-
-let eventFilter = new EventFilter();
+let ruleOrchestration = new RuleOrchestration();
 
 // eslint-disable-next-line no-restricted-globals
 addEventListener("message", ({ data }) => {
@@ -37,28 +15,5 @@ addEventListener("message", ({ data }) => {
     data.value,
     "USER"
   );
-
-  let foundNumbers = [sudokuEvent];
-  do {
-    let cantBeRulesResults = [];
-    for (const rule of numberFound2cantBeRules) {
-      for (const foundNumber of foundNumbers) {
-        cantBeRulesResults = cantBeRulesResults.concat(
-          rule.finderLogic(foundNumber)
-        );
-      }
-    }
-    cantBeRulesResults.forEach(it => {
-      postMessage(it);
-    });
-
-    foundNumbers = [];
-    for (const rule of cantBe2NumberFound) {
-      foundNumbers = foundNumbers.concat(rule.finderLogic(cantBeRulesResults));
-    }
-    foundNumbers = eventFilter.removeAlreadySeen(foundNumbers);
-    foundNumbers.forEach(foundNumbers => {
-      postMessage(foundNumbers);
-    });
-  } while (foundNumbers.length > 0);
+  ruleOrchestration.handleGivenNumber(sudokuEvent, postMessage);
 });
---
+++ src/sudoku/RuleOrchestration.tsx
@@ -0,0 +1,59 @@
+import BoxCantBeRule from "../finder/numberFound2cantBe/BoxCantBeRule";
+import HorizontalCantBeRule from "../finder/numberFound2cantBe/HorizontalCantBeRule";
+import VerticalCantBeRule from "../finder/numberFound2cantBe/VerticalCantBeRule";
+import OnePositonCantBeRule from "../finder/numberFound2cantBe/OnePositonCantBeRule";
+import OnlyOnePlaceHorizontalLine from "../finder/cantBe2NumberFound/OnlyOnePlaceHorizontalLine";
+import OnlyOnePlaceVerticalLine from "../finder/cantBe2NumberFound/OnlyOnePlaceVerticalLine";
+import OnlyOnePlaceBox from "../finder/cantBe2NumberFound/OnlyOnePlaceBox";
+import OnePositionFinder from "../finder/cantBe2NumberFound/OnePositionFinder";
+import EventFilter from "./EventFilter";
+import SudokuEvent from "./SudokuEvent";
+
+let numberFound2cantBeRules = [
+  new BoxCantBeRule(),
+  new HorizontalCantBeRule(),
+  new VerticalCantBeRule(),
+  new OnePositonCantBeRule()
+];
+
+let cantBe2NumberFound = [
+  new OnlyOnePlaceHorizontalLine(),
+  new OnlyOnePlaceVerticalLine(),
+  new OnlyOnePlaceBox(),
+  new OnePositionFinder()
+];
+
+let eventFilter = new EventFilter();
+
+export default class RuleOrchestration {
+  handleGivenNumber(
+    sudokuEvent: SudokuEvent,
+    postMessage: (e: SudokuEvent) => void
+  ) {
+    let foundNumbers = [sudokuEvent];
+    do {
+      let cantBeRulesResults: SudokuEvent[] = [];
+      for (const rule of numberFound2cantBeRules) {
+        for (const foundNumber of foundNumbers) {
+          cantBeRulesResults = cantBeRulesResults.concat(
+            rule.finderLogic(foundNumber)
+          );
+        }
+      }
+      cantBeRulesResults.forEach(it => {
+        postMessage(it);
+      });
+
+      foundNumbers = [];
+      for (const rule of cantBe2NumberFound) {
+        foundNumbers = foundNumbers.concat(
+          rule.finderLogic(cantBeRulesResults)
+        );
+      }
+      foundNumbers = eventFilter.removeAlreadySeen(foundNumbers);
+      foundNumbers.forEach(foundNumbers => {
+        postMessage(foundNumbers);
+      });
+    } while (foundNumbers.length > 0);
+  }
+}
