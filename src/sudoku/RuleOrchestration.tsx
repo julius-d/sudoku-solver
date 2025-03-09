fd7d1e06dbcd0b69236673ce36d343aee65533b3
Julius <30121440+julius-d@users.noreply.github.com>
2025-03-09 22:44:02 +0100

    feat: add cantBe2cantBe

--- src/sudoku/RuleOrchestration.tsx
+++ src/sudoku/RuleOrchestration.tsx
@@ -11,49 +11,73 @@ import CantBeFoundEvent from "./CantBeFoundEvent";
 import NumberFound2CantBe from "../finder/numberFound2cantBe/NumberFound2CantBe";
 import NumberFoundEvent from "./NumberFoundEvent";
 import CantBe2NumberFound from "../finder/cantBe2NumberFound/CantBe2NumberFound";
+import CantBe2CantBe from "../finder/cantBe2cantBe/CantBe2CantBe";
+import PairBasedExcluderInVerticalLine from "../finder/cantBe2cantBe/PairBasedExcluderInVerticalLine";
 
-let numberFound2cantBeRules: NumberFound2CantBe[] = [
-  new BoxCantBeRule(),
-  new HorizontalCantBeRule(),
-  new VerticalCantBeRule(),
-  new OnePositionCantBeRule()
-];
-
-let cantBe2NumberFound: CantBe2NumberFound[] = [
-  new OnlyOnePlaceHorizontalLine(),
-  new OnlyOnePlaceVerticalLine(),
-  new OnlyOnePlaceBox(),
-  new OnePositionFinder()
-];
-
-let eventFilter = new EventFilter();
 
 export default class RuleOrchestration {
+
+  private numberFound2cantBeRules: NumberFound2CantBe[];
+  private cantBe2NumberFound: CantBe2NumberFound[];
+  private cantBe2CantBe: CantBe2CantBe[];
+  private eventFilter: EventFilter;
+
+  constructor() {
+    this.numberFound2cantBeRules = [
+      new BoxCantBeRule(),
+      new HorizontalCantBeRule(),
+      new VerticalCantBeRule(),
+      new OnePositionCantBeRule()
+    ];
+
+    this.cantBe2NumberFound = [
+      new OnlyOnePlaceHorizontalLine(),
+      new OnlyOnePlaceVerticalLine(),
+      new OnlyOnePlaceBox(),
+      new OnePositionFinder()
+    ];
+
+    this.cantBe2CantBe = [
+      new PairBasedExcluderInVerticalLine()
+    ]
+
+    this.eventFilter = new EventFilter();
+  }
+
   handleGivenNumber(
     numberFoundEvent: NumberFoundEvent,
     postMessage: (e: CantBeFoundEvent | NumberFoundEvent) => void
   ) {
-    let foundNumbers = eventFilter.removeAlreadySeen([numberFoundEvent]);
+    let foundNumbers = this.eventFilter.removeAlreadySeen([numberFoundEvent]);
     do {
       let cantBeRulesResults: CantBeFoundEvent[] = [];
-      for (const rule of numberFound2cantBeRules) {
+      for (const rule of this.numberFound2cantBeRules) {
         for (const foundNumber of foundNumbers) {
           cantBeRulesResults = cantBeRulesResults.concat(
             rule.finderLogic(foundNumber)
           );
         }
       }
+
+      for (const rule of this.cantBe2CantBe) {
+        for (const cantBe of cantBeRulesResults) {
+          cantBeRulesResults = cantBeRulesResults.concat(
+            rule.finderLogic(cantBeRulesResults)
+          );
+        }
+      }
+
       cantBeRulesResults.forEach(it => {
         postMessage(it);
       });
 
       foundNumbers = [];
-      for (const rule of cantBe2NumberFound) {
+      for (const rule of this.cantBe2NumberFound) {
         foundNumbers = foundNumbers.concat(
           rule.finderLogic(cantBeRulesResults)
         );
       }
-      foundNumbers = eventFilter.removeAlreadySeen(foundNumbers);
+      foundNumbers = this.eventFilter.removeAlreadySeen(foundNumbers);
       foundNumbers.forEach(foundNumbers => {
         postMessage(foundNumbers);
       });
